b61abc8ec5ee0f09936baddd6715aa2bf04ecd5c
razeeth11 <razeeth011@gmail.com>
2023-05-15 18:14:36 +0530

    Bot & User - UI

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react"
 
 export function Chatbot() {
+
+  const [message,setMessage] = useState([
+    {
+      message : "Hello, Can i know your name ?"
+    }
+  ])
 
   return( 
 
   <div className="chatbot">
     <div className="chat-brand">
-      <img src="src/assets/chatbot.png" alt="" width='50px' height='50px'/>
+      <img src="./src/assets/chatbot.png" alt="" width='50px' height='50px'/>
       <h1>Chat-Bot</h1>
+    </div>
+    {message.map((data,index)=> <ChatMessage props={data} key={index}/> )}
+    <div className="input">
+      <input type="text" placeholder="Enter your query ..." />
+      <button>Send</button>
     </div>
   </div>
   
+  )
+}
+
+export function ChatMessage({props}){
+  console.log(props)
+  return(
+    <div className="chat-message">
+     {props.user ? (
+      <span className="user-message">
+        <p>{props.message}</p>
+        <img src="./src/assets/user.png" alt="" width='30px'/>
+      </span>
+     ) : 
+     <span className="bot-message">
+        <img src="./src/assets/chatbot.png" alt="" width='30px'/>
+        <p>{props.message}</p>
+      </span>
+      }
+    </div>
   )
 }
