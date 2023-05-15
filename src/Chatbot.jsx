cd977e67c6a4be9fc24c6acb09fec86dee1226cb
razeeth11 <razeeth011@gmail.com>
2023-05-15 21:15:24 +0530

    Search-Engine

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,45 +1,70 @@
-import { useState } from "react"
+import { useState } from "react";
+import EcomSearch from '@ecomplus/search-engine'
 
 export function Chatbot() {
-
-  const [message,setMessage] = useState([
+  const [message, setMessage] = useState([
     {
-      message : "Hello, Can i know your name ?"
+      message: "Hello, Can i know your name ?",
+    },
+  ]);
+
+  const [userMessage, setUserMessage] = useState("");
+
+  const onSend = () => {
+    let list = [...message, { message: userMessage, user: true }];
+    console.log(list);
+    if (list.length > 2) {
+    } else {
+      list = [
+        ...list,
+        {
+          message: `Hi ${userMessage}`
+        },
+        {
+          message : "How can i help you ?"
+        }
+      ];
     }
-  ])
+    setMessage(list)
+    setUserMessage("")
+  };
 
-  return( 
-
-  <div className="chatbot">
-    <div className="chat-brand">
-      <img src="./src/assets/chatbot.png" alt="" width='50px' height='50px'/>
-      <h1>Chat-Bot</h1>
+  return (
+    <div className="chatbot">
+      <div className="chat-brand">
+        <img src="./src/assets/chatbot.png" alt="" width="50px" height="50px" />
+        <h1>Chat-Bot</h1>
+      </div>
+      {message.map((data, index) => (
+        <ChatMessage props={data} key={index} />
+      ))}
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Enter your query ..."
+          value={userMessage}
+          onChange={(eve) => setUserMessage(eve.target.value)}
+        />
+        <button onClick={userMessage != "" ? onSend : null}>Send</button>
+      </div>
     </div>
-    {message.map((data,index)=> <ChatMessage props={data} key={index}/> )}
-    <div className="input">
-      <input type="text" placeholder="Enter your query ..." />
-      <button>Send</button>
-    </div>
-  </div>
-  
-  )
+  );
 }
 
-export function ChatMessage({props}){
-  console.log(props)
-  return(
+export function ChatMessage({ props }) {
+  return (
     <div className="chat-message">
-     {props.user ? (
-      <span className="user-message">
-        <p>{props.message}</p>
-        <img src="./src/assets/user.png" alt="" width='30px'/>
-      </span>
-     ) : 
-     <span className="bot-message">
-        <img src="./src/assets/chatbot.png" alt="" width='30px'/>
-        <p>{props.message}</p>
-      </span>
-      }
+      {props.user ? (
+        <span className="user-message">
+          <p>{props.message}</p>
+          <img src="./src/assets/user.png" alt="" width="30px" />
+        </span>
+      ) : (
+        <span className="bot-message">
+          <img src="./src/assets/chatbot.png" alt="" width="30px" />
+          <p>{props.message}</p>
+        </span>
+      )}
     </div>
-  )
+  );
 }
